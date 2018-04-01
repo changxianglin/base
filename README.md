9b75232cd3c0e8b95725eb0def86425e77aaa1c1
changxianglin <281532612@qq.com>
2018-04-01 23:26:36 +0800

    some base knowledga

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 Fe basis
 
 ## learn fe base
++ some base knowledge
---
+++ typeof.js
@@ -0,0 +1,6 @@
+function __main() {
+  js 基本的数据类型通过 typeof 后是小写
+  'string', 'number', 'boolean', 'object', 'undefined'
+  js 语法中定义了些基本对象出现时首字母大写
+  如：'String', 'Number', 'Boolean' 'Array', 'Object', 'Function', 'Date', 'RegExp'等等
+}
