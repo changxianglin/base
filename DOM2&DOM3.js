2a9cd07a50225397618f88178edf40eae8689b91
zhangxianglin <281532612@qq.com>
2018-04-02 09:49:38 +0800

    DOM2&

---
+++ DOM2&DOM3.js
@@ -0,0 +1,54 @@
+function __main() {
+  //  Node 类型变化
+  localName
+  namespaceURI
+  perfix
+  //  Document 类型变化
+  createEelmentNS
+  createAttributeNS
+  getElementsByTagNameNS
+  //  Element 类型变化
+  getAttributeNS
+  getAttributeNodeNS
+  getElementsByTagNameNS
+  hasAttributeNS
+  removeAttributeNS
+  setAttributeNS
+  setAttributeNodeNS
+  // NamedNodeMap 类型变化
+  getNamedItemsNS
+  removeNamedItemNS
+  setNamedItemNS
+  //  css 与 js
+  background-image sytle.backgroundImage
+  color style.color
+  display style.display
+  font-family style.fontFamily
+  // 操作样式
+  disabled 是否被禁用
+  href
+  media
+  ownerNode
+  // 元素大小
+  offsetHeight
+  offsetWidth
+  offsetLeft
+  offsetTop
+  // 客户区大小
+  clientWidth
+  clientHeight
+  // 滚动大小
+  scrollHeight
+  scrollWidht
+  scrollLeft
+  scrollTop
+  // 遍历元素
+  nextNode
+  previousNode
+  // 关系节点
+  parentNode  // 当前节点的父节点
+  firstChild  // 当前节点的第一个子节点
+  lastChild   // 当前节点的最后一个子节点
+  nextSibling // 同辈节点的下一个节点
+  prevousSibling // 同辈节点的上一个节点
+}
