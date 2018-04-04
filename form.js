f8c7463cef11889b59500f2133318f6c7fc19b73
changxianglin <281532612@qq.com>
2018-04-04 22:42:50 +0800

    submit&reset

--- form.js
+++ form.js
@@ -1,3 +1,32 @@
 var __main = function() {
-  
+  // 表单的基础知识
+  // acceptCharset
+  // action
+  // elements
+  // enctype
+  // length
+  // method
+  // name
+  // reset
+  // submit
+  // target
+
+  // 提交表单
+
+  // input 或者 button 的 type = 'submit' 均可提交
+  // var form = document.getElementById('myform')
+  // form.submit() 可提交表单
+
+  // 重置表单
+
+  // type = 'reset' 时 input 和 button 都可以创建重置表单
+
+  // 表单字段
+
+  // 共有的表单字段属性
+  // disable
+  // myform
+  // readOnly
+  // type
+  // value
 }
