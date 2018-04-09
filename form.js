9656e7a1d9d0d2c4b71cd676a5534d9cde6658f3
zhangxianglin <281532612@qq.com>
2018-04-09 09:36:56 +0800

    form&HTML5

--- form.js
+++ form.js
@@ -29,4 +29,61 @@ var __main = function() {
   // readOnly
   // type
   // value
+
+  // 通过 Javascript 动态改变属性
+  // 不推荐改变 type
+
+  // 共有的表单字段方法
+  // focus
+  // blur
+
+  // 共有的表单字段事件
+  // blur
+  // change
+  // focus
+  // input & textarea 从获取焦点到失去焦点且 value 值改变才会触发 change 事件
+  // select 元素用户选择不同的选项就会触发 change 事件
+
+  // 文本框脚本
+  // input
+  // textarea
+
+  // 选择文本
+  // select
+  // 选择 select 事件
+  // selectionStart & selectionEnd
+
+  // 操作剪贴板
+  // beforecopy
+  // copy
+  // beforecut
+  // cut
+  // beforepaste
+  // paste
+
+  // HTML5 约束验证 API
+  // 必填字段 required
+  // 其他输入 input 类型 email & url
+  // 数值范围 number、range、datetime、datetime-local、date、month、week
+  // min & max
+
+  // 输入模式
+  // pattern 类似于正则表达式
+
+  // 检测有效性
+  // checkValidity
+
+  // 选择框脚本
+  // add
+  // multipe
+  // options
+  // remove
+  // selectedIndex
+  // size
+
+  // insertBefore
+  // removeChild
+
+  // 表单序列化
+  
 }
