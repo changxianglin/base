ef67ebe79c597a2b4f08ef2e3d587d3e78608ea0
changxianglin <281532612@qq.com>
2018-04-12 22:07:54 +0800

    JSON

--- JSON&Ajax.js
+++ JSON&Ajax.js
@@ -20,5 +20,45 @@ function __main() {
   // statusText
 
   // readystatechange
+
+  // http 头部信息
+  // Accept
+  // Accept-Charset
+  // Accept-Encoding
+  // Accept-Language
+  // Connnection
+  // Cookie
+  // Host
+  // Referer
+
+  // 先使用 xhr.open() 后使用 xhr.send()
+
+  // get 请求 名值对 由 & 分隔
+  // pot 请求
+
+
+  // XMLHttpRequest 2 级
+
+  // FormData
+  // 超时设定
+
+  // 进度事件
+
+  // loadstart
+  // progress
+  // error
+  // abort
+  // load
+  // loadend
+
+
+  // 跨域源资源共享 cors
+
+  // 其他跨域技术
+  // 图像 Ping
+  // JSONP
+  // Comet
+  // 服务器发送事件 SSE
+  // webSockets
   
 }
