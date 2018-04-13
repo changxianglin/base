c18e42097f43fe74325362da3594e6623018eadd
zhangxianglin <281532612@qq.com>
2018-04-13 18:16:26 +0800

    advanced function

--- advanced-skills.js
+++ advanced-skills.js
@@ -38,4 +38,48 @@ var _main = function() {
       alert(this.message)
     }
   }
+
+  // 函数柯里化
+  function add(num1, num2) {
+    return num1 + num2
+  }
+
+  function curriedAdd(num2) {
+    return add(5, num2)
+  }
+
+  // 创建柯里化函数的通用方法
+  function curry(fn) {
+    var args = Array.prototype.slice.call(arguments, 1)
+    return function() {
+      var innerArgs = Array.prototype.slice.call(arguments)
+      var finalArgs = args.concat(innerArgs)
+      return fn.apply(null, finalArgs)
+    }
+  }
+
+  // 高级应用
+  function bind(fn, context) {
+    var args = Array.prototype.slice.call(arguments, 2)
+    return function() {
+      var innerArgs = Array.prototype.slice.call(arguments)
+      var finalArgs = argus.concat(innerArgs)
+      return fn.apply(context, finalArgs)
+    }
+  }
+
+  // 柯里化函数和动态绑定函数提供了强大的动态函数创建功能，
+  // bind 和 curry 切勿滥用
+
+  // 防止篡改对象
+  // 不可扩展对象 preventExtentsion
+
+  // 密封的对象
+  // Object.seal() Object.isSeal()
+
+  // 冻结的对象
+  // Obejct.freeze() Obejct.isFreeze()
+
+  // 高级定时器
+  
 }
