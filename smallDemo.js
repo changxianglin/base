cbb53fbbdc749feb96ae18f40d68741158a5a754
changxianglin <281532612@qq.com>
2018-04-25 23:54:35 +0800

    write small demo

--- smallDemo.js
+++ smallDemo.js
@@ -8,3 +8,27 @@ var myObject = (function() {
 })()
 console.log(myObject.getName())
 console.log(myObject.__name)
+
+
+var Plane = function() {
+  this.blood = 100
+  this.attackLevel = 1
+  this.defenseLevel = 1
+}
+
+var plane = new Plane()
+plane.blood = 250
+plane.attackLevel = 100
+plane.defenseLevel = 500
+
+
+var user = (function() {
+  var __name = 'Hello'
+  var __gender = 'world'
+
+  return {
+    sayHi: function() {
+      return __name + '--' + __gender
+    }
+  }
+})()
