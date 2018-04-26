173e3c8dd5ac217cdfadf9526f40992bca8b2e9e
zhangxianglin <281532612@qq.com>
2018-04-26 09:36:32 +0800

    sciprted document

---
+++ ScriptedDocuemnt.js
@@ -0,0 +1,27 @@
+var __main = function() {
+  // 通过 ID 选取元素
+  // 通过名字选取元素
+  // 通过标签名选取元素
+  // 节点列表和 HTML 集合 getEelmentsByName()、getElementsByTagName()
+  // 通过 CSS 类选取元素
+  // querySelectorAll()、querySelector()
+
+  // 文档结构和遍历
+  // 作为节点树的文档
+  // parentNode
+  // childNodes
+  // firstChild、lastChild
+  // nextSibling、previoursSibling
+  // nodeType
+  // nodeValue
+  // nodeName
+
+  // 属性
+  // HTML 属性作为 Element 的属性
+  // 获取和设置非标准 HTML 属性
+  // getAttribute()、setAttribute()
+  // hasAttribute()、removeAttribute()
+
+  // 数据属性
+  
+}
