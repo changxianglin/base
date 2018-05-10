6a1411a47a111706de2c7f2ea36cf005b9a35236
changxianglin <281532612@qq.com>
2018-05-10 23:57:40 +0800

    add some new lore

---
+++ browser.js
@@ -0,0 +1,7 @@
+const main => {
+  // IE Firefox Oprea Chrome Safari 五大浏览器
+  // document.all 从使用到废弃
+  // transition 渐变动画
+  // animation 补间动画
+  // 
+}
---
+++ date.js
@@ -0,0 +1,5 @@
+const main => {
+  // new Date()
+  // new Date(value)传入毫秒数
+  // js 的 Date 实在是有很多工作要做。。。
+}
