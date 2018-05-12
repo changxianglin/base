7d3b2a061846ade0b9fcca7d09ebd314051a0251
changxianglin <281532612@qq.com>
2018-05-12 23:54:48 +0800

    new object

--- likefactory.js
+++ likefactory.js
@@ -10,4 +10,29 @@ const main => {
   // 原型链继承，构造函数继承，组合继承，寄生式继承，寄生组合继承...
 
   // 库 P.js
+
+  // JS Class
+
+  // simple-inheritance
+
+  // 体现 JavaScript 灵活性的库 - def.js
+
+  // 进击的属性描述符
+
+  // ES5 的 Object 添加的心方法
+
+  // Object.keys,Object.getOwnPropertyNames
+  // Object.getPrototypeOf, Object.defineProperty
+  // Object.getOwnPropertyDescriptor
+  // Object.create
+  // Object.seal
+  // Object.freeze
+  // Object.preventExtentsions
+  // Object.isSealed
+  // Object.isFrozen
+  // Object.isExtensible
+
+  // 纯空对象 var object = Object.create(null)
+
+  
 }
