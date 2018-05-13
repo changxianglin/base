cdde6c9107d7e8fbaddde0f1d269d25b6713ab3c
changxianglin <281532612@qq.com>
2018-05-13 23:57:24 +0800

    css select

---
+++ selector.js
@@ -0,0 +1,21 @@
+const main = （）=> {
+  // 选择器引擎
+
+  // getElementById,getElementByTagName 是 IE5 引入的
+  // querySelector,querySelectorAll 是 IE8 引入
+
+  // css 选择包含 4 大类 16 中
+  // 并联选择器
+  // 简单选择器
+  // 关系选择器
+  // 伪类
+
+  // 节点
+  // firstChild，lastChild，previousSibling,nextSibling,parentNode,length
+  // 元素
+  // firstElementChild,lastElementChild,previousElementSibling,parentElement,childElementCount
+
+  // contains
+
+  // Sizzle 引擎
+}
