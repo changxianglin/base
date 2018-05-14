ae809f19f8ba277869d12df3b9d813ac5f750d0a
changxianglin <281532612@qq.com>
2018-05-14 22:55:58 +0800

    node module

---
+++ createElement.js
@@ -0,0 +1,17 @@
+const main = () => {
+  // 节点的创建
+  // document.createElement, innerHTML,insertAdjacentHTML,createContextualFragment
+  // appendChild, insertBefore,
+  // 浏览器会有自动补全闭合标签，body,colgroup,dd,dt,head,html,li,optgroup,option,p,tbody,td,tfoot,th,thead,try,
+  // insertAdjacentHTML 非常灵活，插入元素内部最前面 afterBegin,内部最后面 beforeEnd,元素前面 beforeBegin,后面 afterEnd
+  // 节点的插入
+  // 节点的复制
+  // 节点的移除
+  // removeChild, removeNode
+  // 节点的移除回调实现
+  // innerHTML, innerText, outerHTML, outerText 的兼容处理
+  // 模板容器元素
+  // HTML5 新出品的 template 元素
+  // iframe 元素
+  // 
+}
