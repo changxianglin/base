f8ac8296497955d7ea9c5a1c292a45e3e5c0ec47
changxianglin <281532612@qq.com>
2018-05-14 23:49:10 +0800

    data&style

---
+++ datacacheMoudle.js
@@ -0,0 +1,8 @@
+const main = () => {
+  //  jquery UUID
+  // 基于内存，即代码结构内
+  // 本地存储系统
+  // cookie  在 HTML5 中 5 M 大小
+  // localStorage
+  // 
+}
---
+++ styleModule.js
@@ -0,0 +1,21 @@
+const main = () => {
+  // 精准的获取样式值与设置样式值
+  // 外部样式，内部样式，行内样式，外加一个 important 对于权重的干扰
+  // 样式名修正
+  // 个别样式的特殊处理
+  // opacity
+  // user-select
+  // background-position
+  // z-index
+  // 盒子模型
+  // 元素的尺寸
+  // margin, border, padding, content,
+  // 元素的尺寸
+  // 窗口大小&页面大小
+  // 元素的显隐
+  // display: none,
+  // 例外，thead,tbody,tr
+  // 元素的坐标，指其与 top 与 left 值
+  // 元素滚动条的坐标
+  // 
+}
