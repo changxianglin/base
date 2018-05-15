caa30f3009e20272643741e56878c9066380cce9
changxianglin <281532612@qq.com>
2018-05-15 23:07:58 +0800

    property&attribute

---
+++ property&attribute.js
@@ -0,0 +1,12 @@
+const main = () => {
+  // attribute
+  // property
+  // 节点属性控制
+  // setAttribute,getAttribute,removeAttribute
+  // 如何区分固有属性与自定义属性
+  // 如何判定浏览器是否区分固有属性与自定义属性
+  // IE 的属性系统的 3 次演变
+  // className 的操作
+  // 操作一个属性通常有：设置、读取、删除
+  // HTML 5 classList 拥有 add, remove, contains,toggle
+}
