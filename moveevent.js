acc4bd1abaafd90f1598fa6e7ec190df07436ac1
changxianglin <281532612@qq.com>
2018-05-16 23:49:42 +0800

    mobilephone

---
+++ moveevent.js
@@ -0,0 +1,17 @@
+const main = () => {
+  // 智能手机上的交互基本是通过手机 touch 进行，于是出现
+  // 划动(swipe), 拖动(drag),缩放(pinch)
+  // touchstart
+  // touchmove
+  // touchend
+  // touchcancel
+  // 移动端兼容性非常差（很难做）
+  // getsture 系统
+  // top 系统
+  // press 系统
+  // swipe 系统
+  // pinch 系统
+  // 拖放事件
+  // rotate 系事件（旋转）
+  // 
+}
