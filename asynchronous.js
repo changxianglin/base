605a8d6a8b96e5fecc785a9fdede975096a36eef
changxianglin <281532612@qq.com>
2018-05-17 23:56:19 +0800

    async

---
+++ asynchronous.js
@@ -0,0 +1,16 @@
+const main = () => {
+  // setTimeout
+  // setInterval
+  // 回调函数
+  // clearTimeout
+  // clearInterval
+  // promise 诞生前的世界
+  // 回调函数的 callbacks
+  // 观察者模式 observers
+  // 又叫发布订阅者模式
+  // 原型链， EventTarget
+  // JSDeferred 里程碑
+  // 核心区别
+  // es6 Promise 第一个标准模型
+  // 
+}
