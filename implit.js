8d8b203f5d0a0de0852e9c3ca86e3350811d8685
changxianglin <281532612@qq.com>
2018-05-24 23:49:03 +0800

    paramter function

--- implit.js
+++ implit.js
@@ -4,4 +4,24 @@ const main = () => {
   function isReallyNaN(x) {
     return x !== x
   }
+  //
+  [3, 1, 4, 1, 5, 9].sort(function(x, y) {
+    if(x < y) {
+      return -1
+    }
+    if(x > y) {
+      return 1
+    }
+    return 0
+  })
+
+  // 高阶函数
+  function buidString(n, callback) {
+    var result = ''
+    for(var i = 0; i < n; i++) {
+      result += callback(i)
+    }
+    return result
+  }
+  
 }
