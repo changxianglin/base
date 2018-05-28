a7d6034cc10ff9befd9853c7e925f9f1734ba39a
changxianglin <281532612@qq.com>
2018-05-28 23:51:12 +0800

    understanding promise

--- promise.js
+++ promise.js
@@ -1,5 +1,14 @@
 const main = () => {
   // promise
+  setTimeout(function () {
+    console.log(1)
+  }, 0);
+  new Promise(function executor(resolve) {
+          resolve();
+  }).then(function () {
+      console.log(2);
+  });
+  // 输处是 2， 1 原因是 Promise 是一个整体任务（实例先运行，then再其后），但是 setTimeout 是一个队列任务。
 
   // 闭包
 var foo = function() {
